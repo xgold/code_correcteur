e78556de1ad52f6f7919095ed9cba7938a1a04a4
le déroff <lederoff@hotmail.fr>
2020-05-24 13:14:09 +0200

    Implementation code BCH (18,8,5) shortening of BCH(31,21,5)

--- bch_function.m
+++ bch_function.m
@@ -6,9 +6,19 @@ function [code] = bch_function(data_to_code, code_or_decode)
 % k = Message length
 
 if code_or_decode == 1
-    code = bch_encoder(data_to_code);
+    code = bch_encoder(data_to_code(1:8));
+    % create packet of 8 bit
+    for j = (1:length(data_to_code)/8-1)
+        code = [code, bch_encoder(data_to_code(1+(j*8):8+(j*8)))];
+    end
 elseif code_or_decode == 0
-    code = decode(data_to_code);
+    [data_decoder, nerr] = bch_decoder(data_to_code(1:18));
+    code = data_decoder(1:8);
+    % create packet of 18 bit
+    for j = (1:length(data_to_code)/18-1)
+         [data_decoder,nerr] = bch_decoder(data_to_code(1+(j*18):18+(j*18)));
+         code = [code, data_decoder(1:8)];
+    end
 end
 
 end
